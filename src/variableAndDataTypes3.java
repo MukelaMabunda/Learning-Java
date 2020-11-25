de8c6ee448418730dc2294b1dc6b5b7383aa1db6
Zenith Mabunda <zenith.mabunda01@gmail.com>
2020-11-25 21:16:54 +0200

    local and instance variables

---
+++ src/variableAndDataTypes3.java
@@ -0,0 +1,19 @@
+
+public class variableAndDataTypes3
+{
+	public String instantVar = "instant Var";
+	
+	public void demoFunction()
+	{
+		String instantVar = "Local Var";
+		System.out.println(instantVar);
+	}
+	
+	public static void main(String[] args)
+	{
+		variableAndDataTypes3 obj = new variableAndDataTypes3();
+		System.out.println("Call function");
+		obj.demoFunction();
+		System.out.println(obj.instantVar);
+	}
+}
