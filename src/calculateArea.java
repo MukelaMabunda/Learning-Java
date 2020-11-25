2913021ebd3bc3f87abc43eb2e3f11d5db1f0eba
Zenith Mabunda <zenith.mabunda01@gmail.com>
2020-11-25 22:12:18 +0200

    calculating area of circle in java

---
+++ src/calculateArea.java
@@ -0,0 +1,10 @@
+
+public class calculateArea
+{
+	public static void main(String[] args)
+	{
+		float radius = 2.5f;
+		double area = radius * radius * 3.14159f;
+		System.out.println("The area is " + area + " for radius " + radius);
+	}
+}
