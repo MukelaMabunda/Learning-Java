9a0559891e5cc4ee92805058e3e0531d7bf5c3c2
Zenith Mabunda <zenith.mabunda01@gmail.com>
2020-11-25 20:17:38 +0200

    making use of variables 1.0

---
+++ src/variable.java
@@ -0,0 +1,15 @@
+public class variable 
+{
+	static int staticVar = 1;
+	int instantVar = 2;
+
+	public void functionName(int functionParameter) 
+	{
+		int localVar = 3;
+
+		if (true) 
+		{
+			int blockVar = 4;
+		}
+	}
+}
---
+++ src/variable1.java
@@ -0,0 +1,9 @@
+
+public class variable1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
