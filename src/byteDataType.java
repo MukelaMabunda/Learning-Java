4ff6bb8d01bf06486dafa7fbaec3e5333518829d
Zenith Mabunda <zenith.mabunda01@gmail.com>
2020-11-25 21:30:14 +0200

    couple of datat types

---
+++ src/byteDataType.java
@@ -0,0 +1,27 @@
+
+public class byteDataType
+{
+	public static void main(String[] args)
+	{
+		byte num;
+		num = 123;
+		System.out.println(num);
+		
+		
+		short num2;
+		num2 = 1234;
+		System.out.println(num2);
+		
+		int num3;
+		num3 = 123456;
+		System.out.println(num3);
+		
+		long num4;
+		num4 = 123456789;
+		System.out.println(num4);
+		
+		double num5;
+		num5 = 12345.6789;
+		System.out.println(num5);
+	}
+}
