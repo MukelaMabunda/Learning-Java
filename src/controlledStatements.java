b7832eba0b03971b8d1abe8c7212f07a9fd0ed26
Zenith Mabunda <zenith.mabunda01@gmail.com>
2020-11-27 14:37:27 +0200

    Enumerating the Possibilities

--- src/controlledStatements.java
+++ src/controlledStatements.java
@@ -30,38 +30,63 @@ public class controlledStatements
 	 * System.out.println("Exited ‘while’"); }
 	 */
 
+	/*
+	 * public static void main(String args[]) { try (Scanner myScanner = new
+	 * Scanner(System.in)) { int hankees, socks;
+	 * out.print("Hankees and Socks scores?"); hankees = myScanner.nextInt(); socks
+	 * = myScanner.nextInt(); out.println(); if (hankees > socks) {
+	 * out.println("Hankees win..."); out.print("Hankees: "); out.println(hankees);
+	 * out.print("Socks: "); out.println(socks); } else if (socks > hankees) {
+	 * out.println("Socks win..."); out.print("Socks: "); out.println(socks);
+	 * out.print("Hankees: "); out.println(hankees);
+	 * 
+	 * } else { out.println("It's a tie..."); out.print("Hankees: ");
+	 * out.println(hankees); out.print("Socks: "); out.println(socks); } } }
+	 */
+
 	public static void main(String args[])
 	{
-		try (Scanner myScanner = new Scanner(System.in))
+		Scanner myScanner = new Scanner(System.in);
+		int hankees, socks;
+		WhoWins who;
+		out.print("Hankees and Socks scores? ");
+		hankees = myScanner.nextInt();
+		socks = myScanner.nextInt();
+		out.println();
+		if (hankees > socks)
 		{
-			int hankees, socks;
-			out.print("Hankees and Socks scores?");
-			hankees = myScanner.nextInt();
-			socks = myScanner.nextInt();
-			out.println();
-			if (hankees > socks)
-			{
-				out.println("Hankees win...");
-				out.print("Hankees: ");
-				out.println(hankees);
-				out.print("Socks: ");
-				out.println(socks);
-			} else if (socks > hankees)
-			{
-				out.println("Socks win...");
-				out.print("Socks: ");
-				out.println(socks);
-				out.print("Hankees: ");
-				out.println(hankees);
-
-			} else
-			{
-				out.println("It's a tie...");
-				out.print("Hankees: ");
-				out.println(hankees);
-				out.print("Socks: ");
-				out.println(socks);
-			}
+			who = WhoWins.home;
+			out.println("The Hankees win :-)");
+		} else if (socks > hankees)
+		{
+			who = WhoWins.visitor;
+			out.println("The Socks win :-(");
+		} else
+		{
+			who = WhoWins.neither;
+			out.println("It’s a tie :-|");
+		}
+		out.println();
+		out.println("Today’s game is brought to you by");
+		out.println("SnitSoft, the number one software");
+		out.println("vendor in the Hankeeville area.");
+		out.println("SnitSoft is featured proudly in");
+		out.println("Chapter 6. And remember, four out");
+		out.println("of five doctors recommend SnitSoft");
+		out.println("to their patients.");
+		out.println();
+		if (who == WhoWins.home)
+		{
+			out.println("We beat ‘em good. Didn’t we?");
+		}
+		if (who == WhoWins.visitor)
+		{
+			out.println("The umpire made an unfair call.");
+		}
+		if (who == WhoWins.neither)
+		{
+			out.println("The game goes into overtime.");
 		}
 	}
+
 }
