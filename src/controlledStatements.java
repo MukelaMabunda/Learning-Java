d51fe6bb5f663bb24630cbc7cedb77041073322b
Zenith Mabunda <zenith.mabunda01@gmail.com>
2020-11-27 14:15:31 +0200

    while statements

--- src/controlledStatements.java
+++ src/controlledStatements.java
@@ -1,39 +1,35 @@
 
 public class controlledStatements
 {
-	/*public static void main(String[] args)
-	{
-		int a = 200;
-		int b = 100;
-		if (a > b)
-		{ // if true,run the next command
-			System.out.print("a is greater than b.");
-		}
-	}*/
+	/*
+	 * public static void main(String[] args) { int a = 200; int b = 100; if (a > b)
+	 * { // if true,run the next command System.out.print("a is greater than b."); }
+	 * }
+	 */
 
-	static int result = 0;
-	static void test(int testval, int target)
+	/*
+	 * static int result = 0; static void test(int testval, int target) { if
+	 * (testval > target) result = +1; else if (testval < target) result = -1; else
+	 * result = 0; }
+	 * 
+	 * private static void print(int result2) { System.out.println(result2); }
+	 * 
+	 * public static void main(String[] args) { test(10, 5); print(result); test(5,
+	 * 10); print(result); test(5, 5); print(result); }
+	 */
+
+	static boolean condition()
 	{
-		if (testval > target)
-			result = +1;
-		else if (testval < target)
-			result = -1;
-		else
-			result = 0;
+		boolean result = Math.random() < 0.99;
+		System.out.print(result + ", ");
+		return result;
 	}
-	
-	private static void print(int result2)
-	{
-		System.out.println(result2);
-	}
-	
+
 	public static void main(String[] args)
 	{
-		test(10, 5);
-		print(result);
-		test(5, 10);
-		print(result);
-		test(5, 5);
-		print(result);
+		while (condition())
+			System.out.println("Inside ‘while’");
+		
+		System.out.println("Exited ‘while’");
 	}
 }
