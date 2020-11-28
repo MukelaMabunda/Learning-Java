5762044e79e23eb82f166deb8a5d2ece83507a3e
Zenith Mabunda <zenith.mabunda01@gmail.com>
2020-11-28 11:26:38 +0200

    while loop

--- src/loops.java
+++ src/loops.java
@@ -1,11 +1,14 @@
 
 public class loops
 {
-
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-
+		int a = 0;
+		System.out.println("The Changing value of 'a' during execution :");
+		while (a < 20)
+		{
+			System.out.println("a : " + a);
+			a++;
+		}
 	}
-
 }
